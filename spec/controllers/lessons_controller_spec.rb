77c19a3e372fc577d204fd1495d6263d82358484
Juan David <juandominguez2017@u.northwestern.edu>
2016-03-04 15:35:59 -0600

    failing test for routing

--- spec/controllers/lessons_controller_spec.rb
+++ spec/controllers/lessons_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe LessonsController, type: :controller do
 
+	describe 'GET index' do
+		it 'returns an http success' do
+			get :index
+			expect(response).to be_success
+		end
+
 end
