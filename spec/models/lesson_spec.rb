9264170daae8c3bf7f0c6caf004290fa0c497a12
Juan David <juandominguez2017@u.northwestern.edu>
2016-03-04 17:55:12 -0600

    Adds pending tests to models

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it 'has a factory'
+
+	it 'is invalid without a "creator"'
+
+	it 'is invalid without a body'
+
+	it 'is invalid without a URL'
 end
