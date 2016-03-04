b2856e85fa632df4b90aa137207901e2ec8473ec
Juan David <juandominguez2017@u.northwestern.edu>
2016-03-04 17:47:04 -0600

    Adds new pending specs for lessons_controller

--- spec/controllers/lessons_controller_spec.rb
+++ spec/controllers/lessons_controller_spec.rb
@@ -2,12 +2,41 @@ require 'rails_helper'
 
 RSpec.describe LessonsController, type: :controller do
 
-	describe 'GET index' do
+	describe 'GET / index' do
 		it 'returns an http success' do
 			get :index
 			expect(response).to be_success
 
 		end
+
+		it 'returns all the lessons'
+
+		it 'shows all the lessons'
+
+		it 'has a summary of each lesson'
+	end
+
+	describe 'GET /# show' do
+		it 'returns a single lesson'
+
+		it 'returns the correct lesson'
+
+		it 'shows a body for the lesson'
+
+		it 'shows the creator of the lesson'
+
+		it 'shows the creator of the lesson'
+
+	end
+
+	describe 'GET /new create' do
+		it 'shows a form for a new lesson'
+
+		it 'adds a new lesson when form is submitted'
+
+		it 'does not repeat lesson urls'
+
+		it 'does not allow to submit a lesson unless everything in the form is filled out'
 	end
 
 end
