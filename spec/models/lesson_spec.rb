9bbe744c0059fc2e3bc5c358b09967e7c6b435f8
Juan David <juandominguez2017@u.northwestern.edu>
2016-03-05 12:39:13 -0600

    specs for lesson models

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
-	it 'has a factory'
+	it 'has a factory' do
+		expect(FactoryGirl.create(:lesson)).to be_valid
+	end
 
-	it 'is invalid without a "creator"'
+	it 'is invalid without a "author"' do
+		expect(FactoryGirl.build(:lesson, author: nil)).to_not be_valid
+		expect(FactoryGirl.build(:lesson, author: '')).to_not be_valid
+	end
 
-	it 'is invalid without a body'
+	it 'is invalid without a body' do
+		expect(FactoryGirl.build(:lesson, body: nil)).to_not be_valid
+		expect(FactoryGirl.build(:lesson, body: '')).to_not be_valid
+	end
 
-	it 'is invalid without a URL'
+	it 'is invalid without a url' do
+		expect(FactoryGirl.build(:lesson, url: nil)).to_not be_valid
+		expect(FactoryGirl.build(:lesson, url: '')).to_not be_valid
+	end
+		
+
+	it 'is invalid without a title' do
+		expect(FactoryGirl.build(:lesson, title: nil)).to_not be_valid
+		expect(FactoryGirl.build(:lesson, title: '')).to_not be_valid
+	end
 end
