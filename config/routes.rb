d75d33bada56d3b457bad8db9ed0588ca5459227
Juan David <juandominguez2017@u.northwestern.edu>
2016-04-02 14:28:26 -0500

    refactors routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-	
+  root to: "home#index"
+  devise_for :users
 	resources :lessons
-    root to: "home#index"
     
-    devise_for :users 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
