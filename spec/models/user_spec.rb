642e377a5e310df66c1f87af3839f7d187ae5ef3
Juan David <juandominguez2017@u.northwestern.edu>
2016-04-02 14:35:57 -0500

    Adds user pended tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe User, type: :model do
+	it 'has a factory'
+
+	it 'is invalid without an "email"'
+
+	it 'is invalid without a full name'
+
+	it 'is invalid without a password'
+
+	it 'is invalid without a role'
+
+	it 'has a default role of student'
+
+	it 'can have an admin role'
+
+end
