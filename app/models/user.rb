bcb4fe421d03f8dd90327e5fb99d74a48760f217
Steven Layne <stevenlayne2017@u.northwestern.edu>
2016-04-04 01:24:44 +0000

    added role to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  enum role: [:admin, :student]
+  
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	validates :first_name, :last_name, presence: true
+	validates :first_name, :last_name, :role, presence: true
 	
+	def set_default_role
+	  self.role ||= :student
+	end 
+
 end
