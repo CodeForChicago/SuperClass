9e3feaf00fbc762d5b757cc9c8ffb81cadbb96f5
bensandeen <benjaminsandeen2016@u.northwestern.edu>
2016-04-05 01:36:03 +0000

    Cleaning up files

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,11 +6,8 @@ class LessonsController < ApplicationController
 	end
 	
 	def show # show has an implicit id argument (and probably other arguments too)
-		# @lesson = Lesson.first # This will likely need to be changed to 
-		# binding.pry
-		
 		# Used the below format for getting the correct lesson
-		# @lesson = Lesson.find(:id)
+		# @lesson = Lesson.find(id_num)
 		
 		# Here's the actual code for doing so; it's ugly, but it passes our tests thus far
 		@lesson = Lesson.find(self.request.filtered_parameters["id"])
