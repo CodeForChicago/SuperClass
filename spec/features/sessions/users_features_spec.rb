fe351e88d9923640fa28955f282c798da1d6ca54
Akshay Batra <akshaybatra2018@u.northwestern.edu>
2016-04-09 19:04:50 +0000

    added sign in test

---
+++ spec/features/sessions/users_features_spec.rb
@@ -0,0 +1,32 @@
+RSpec.configure do |c|
+	  c.include Helpers::SessionsHelper
+end
+
+# Feature: Sign in 
+# As a user
+# I want to sign in 
+# So I can visit protected areas of the site
+
+feature 'Sign in', :devise do
+  
+  #Scenario: User cannot sign in if not registered\
+  scenario 'user cannot sign in if not registered' do
+    visit new_user_session_path
+    signin('stevo@fakemail.com', 'password123')
+    click_button 'Log in'
+    expect(page).to have_content I18n.t 'devise.failure.not_found_in_database', authentication_keys: 'email'
+  end 
+  
+  #Scenario: User can sign in if registered
+  scenario 'user can sign in if registered' do
+    visit new_user_session_path
+    user = FactoryGirl.create(:user)
+    signin(user.email, user.password)
+    # signin('livinglife@gmil.com', 'smdfreestyle')
+    click_button 'Log in'
+    expect(page).to have_content I18n.t 'devise.sessions.signed_in' 
+  end 
+
+  #Scenario: User cannot sign in if email/password combination incorrect 
+  
+end
