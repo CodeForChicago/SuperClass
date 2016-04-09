10967524f24baa537304ddc46c9b44b18b2decb3
Michael Horst <michaelhorst2016@u.northwestern.edu>
2016-04-09 20:41:30 +0000

    Updated spec tests for questions_feature.

--- spec/features/questions_feature_spec.rb
+++ spec/features/questions_feature_spec.rb
@@ -8,9 +8,11 @@ feature "Questions Page" do
         visit questions_path
         expect(page).to have_content(question1.title)
         expect(page).to have_content(question1.body)
-        expect(page).to have_content(question1.user)
+        expect(page).to have_content(question1.user.first_name)
+        expect(page).to have_content(question1.user.last_name)
         expect(page).to have_content(question2.title)
         expect(page).to have_content(question2.body)
-        expect(page).to have_content(question2.user)
+        expect(page).to have_content(question2.user.first_name)
+        expect(page).to have_content(question2.user.last_name)
     end
 end
