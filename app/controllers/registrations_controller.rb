5222a25063e8bcbacbbe0e20cece1c559586bec7
Steven Layne <stevenlayne2017@u.northwestern.edu>
2016-04-09 17:37:34 +0000

    Separated the device controller from the application controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < Devise::RegistrationsController
+  protect_from_forgery with: :exception
+  
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  
+  protected 
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:first_name ,:last_name, :password, :password_confirmation, :email)}
+  end
+end
