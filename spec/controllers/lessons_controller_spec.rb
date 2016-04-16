a1aa477f7dd437e25049c3b36bfe1c73585a5866
Juan David <juandominguez2017@u.northwestern.edu>
2016-04-16 13:08:48 -0500

    Removes unnecessary tests

--- spec/controllers/lessons_controller_spec.rb
+++ spec/controllers/lessons_controller_spec.rb
@@ -14,11 +14,8 @@ RSpec.describe LessonsController, type: :controller do
 
 		it 'returns all the lessons' do
 			get :index 
-			expect(assigns[:lessons]).to eq([])
-			# this causes the lessons to be created 
 			lessons = [lesson1, lesson2]
-			# use `.reload` to force a new query:
-			expect(assigns[:lessons].reload).to eq(lessons)
+			expect(assigns[:lessons]).to eq(lessons)
 		end
 	end
 
@@ -40,27 +37,12 @@ RSpec.describe LessonsController, type: :controller do
 	end
 
 	describe 'GET /new' do
-		it 'returns an http success when form loaded' do
-			get :new
-			expect(response).to be_success
-		end
-		
-		it 'returns a valid form, with fields for all necessary information' do
-			
-		end
+		it 'returns an http success when form loaded'		
+		it 'returns a valid form, with fields for all necessary information'
 	end
 
 	describe 'POST / create' do
-		it 'returns an http created when lesson saved' do
-			get :create, author: lesson1.author, title: lesson1.title,\
-					body: lesson1.body, url: lesson1.url
-			# binding.pry
-			# returns http status code 201?
-			# there's probably some better way, but I don't know it
-			# response.code gets http code
-			expect(response.code).to eq("201")
-		end
-
+		it 'returns an http created when lesson saved' 
 		it 'returns an http failed when lesson not saved'
 
 		it 'returns model message when lesson not saved'
@@ -69,9 +51,7 @@ RSpec.describe LessonsController, type: :controller do
 
 		it 'keeps the admin on the form to create a new one'
 		
-		# this one may be taken care of by the lesson model, so we may not need to
-		# test it here
-		it 'doesn\'t create a duplicate lesson'
+		it 'does not create a duplicate lesson'
 
 	end
 
