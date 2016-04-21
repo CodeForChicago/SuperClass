51a26d93152a7927bc7e5fd93a36d7a543286eb7
Akshay Batra <akshaybatra2018@u.northwestern.edu>
2016-04-21 02:38:29 +0000

    Spec for Orgleader

---
+++ spec/features/orgleader_add_feature_spec.rb
@@ -0,0 +1,21 @@
+# Feature: If user is orgleader then user can add student
+
+feature 'Add Student', :devise do
+  
+  scenario 'if you are org leader then you can add student' do 
+    visit add_student_path
+    expect(current_user.org_leader?).to be_true
+    click_button 'Add Student'
+    #Discuss with Juan what we get when we select roles
+    expect(page.status_code).to eq(200)
+  end 
+  
+  scenario 'if you are not org leader then cant access page' do
+    visit add_student_path
+    #checking to see if current_user is an org leader
+    expect(current_user.org_leader?).to be_false
+    click_button 'Add Student'
+    expect(page.status_code).to eq(404)
+  end 
+  
+end 
