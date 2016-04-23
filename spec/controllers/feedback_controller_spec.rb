671319ddf36688be2a3128124f9653e77d1084ff
Diego Lopez <diegolopez2019@u.northwestern.edu>
2016-04-23 18:09:32 +0000

    Initialized testing for feedback page. Removed ineffective tests from feedback_controller

--- spec/controllers/feedback_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-
-require "spec_helper"
-
-feature "feedback_page" do
-    scenario "user sends feedback" do
-        visit "/feedback"
-    end
-end
-
--- spec/features/feedback_feature_spec.rb
+++ spec/features/feedback_feature_spec.rb
@@ -2,15 +2,9 @@ require 'spec_helper'
 require "pry"
 
 feature "Feedback Page" do
-	let!(:feedback1) {FactoryGirl.create(:feedback)}
 	
-		scenario "viewing feedback page" do
-		visit lessons_path
-		puts lessons_path
-		expect(page).to have_content(feedback1.title)
-		expect(page).to have_content(feedback1.title)
-		expect(page).to have_content(feedback1.body)
-		expect(page).to have_content(feedback1.body)
+	scenario "viewing feedback page" do
+		visit feedback_path
 	end
 	
 end 
