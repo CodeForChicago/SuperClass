74871d5baf56002d53f25775144cab79d04bcb9e
Diego Lopez <diegolopez2019@u.northwestern.edu>
2016-04-23 19:12:20 +0000

    Check to see if user can fill in the feedback form

--- spec/features/feedback_feature_spec.rb
+++ spec/features/feedback_feature_spec.rb
@@ -3,8 +3,13 @@ require "pry"
 
 feature "Feedback Page" do
 	
-	scenario "viewing feedback page" do
+	scenario "user is not signed in" do
 		visit feedback_path
+		fill_in 'Email', with: 'manchesterisred@cfc.com'
+		fill_in 'Name', with: 'wayne rooney'
+		fill_in 'Feedback', with: 'manchester is red'
+		click_button 'Send message'
+		expect(page).to have_content 'We appreciate your feedback!'
 	end
 	
 end 
