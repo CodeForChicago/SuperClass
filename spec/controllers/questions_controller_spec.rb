f2fa18fa2e0301c37b80a42be6030428c66ad18a
Michael Horst <michaelhorst2016@u.northwestern.edu>
2016-04-28 02:10:32 +0000

    Added tests to questions controller spec for loading specific question pages

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -22,5 +22,15 @@ describe QuestionsController, type: :controller do
            get :show, id: question1.id
            expect(response).to be_success
        end
+       
+       it 'returns a single question' do
+           get :show, id: question1.id
+           expect(assigns[:question]).to eq(question1)
+       end
+       
+       it 'returns the correct question' do
+           get :show, id: question2.id
+           expect(assigns[:question]).to eq(question2)
+       end
     end
 end
