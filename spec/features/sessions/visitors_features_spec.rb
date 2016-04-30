9f0f4cab9274e28f5fa4c35a85d6744e203fbff0
Akshay Batra <akshaybatra2018@u.northwestern.edu>
2016-04-30 18:11:49 +0000

    Trying to fix visitors_features

--- spec/features/sessions/visitors_features_spec.rb
+++ spec/features/sessions/visitors_features_spec.rb
@@ -7,10 +7,11 @@ feature 'Sign Up' do
 	l_name = "Chicageous"
 	password = "cfcrulez"
 	email = "datcfc@gmail.com"
+	role = "Student"
     
     scenario 'visitor can sign up with valid email address and password' do
         visit new_user_registration_path
-				sign_up_with(email, password, password, f_name, l_name)
+				sign_up_with(email, password, password, f_name, l_name, role)
  				click_button 'Sign up'
  				txts = [I18n.t( 'devise.registrations.signed_up'), I18n.t( 'devise.registrations.signed_up_but_unconfirmed')]
   			expect(page).to have_content(/.*#{txts[0]}.*|.*#{txts[1]}.*/)
@@ -18,28 +19,28 @@ feature 'Sign Up' do
     
     scenario 'visitor cannot sign up without a valid email address' do
       visit new_user_registration_path
-			sign_up_with("bogus", password, password, f_name, l_name)
+			sign_up_with("bogus", password, password, f_name, l_name, role)
  			click_button 'Sign up'
 			expect(page).to have_content 'Email is invalid'
 		end
     
     scenario 'visitor cannot sign up with a blank password' do
       visit new_user_registration_path
-			sign_up_with("bogus", '', '', f_name, l_name)
+			sign_up_with("bogus", '', '', f_name, l_name, role)
  			click_button 'Sign up'
 			expect(page).to have_content "Password can't be blank"
 		end
     
     scenario 'visitor cannot sign up with a short password' do
       visit new_user_registration_path
-			sign_up_with(email, 'bruh', 'bruh', f_name, l_name)
+			sign_up_with(email, 'bruh', 'bruh', f_name, l_name, role)
  			click_button 'Sign up'
 			expect(page).to have_content "Password is too short"
-		end	
+		end
     
     scenario 'visitor cannot sign up with an invalid password confirmation' do #Test for mismatch and no confirmation 
       visit new_user_registration_path
-			sign_up_with(email, password, 'bruh', f_name, l_name)
+			sign_up_with(email, password, 'bruh', f_name, l_name, role)
  			click_button 'Sign up'
 			expect(page).to have_content "Password confirmation doesn't match"
 		end
--- spec/support/sessions/sessions_helper.rb
+++ spec/support/sessions/sessions_helper.rb
@@ -1,19 +1,18 @@
 module Helpers
 	module SessionsHelper
-		def sign_up_with(email, password, password_confirmation, f_name, l_name)
+		def sign_up_with(email, password, password_confirmation, f_name, l_name, role = nil)
         fill_in 'Email', with: email
         fill_in 'Password', with: password
         fill_in 'Password confirmation', with: password_confirmation
         fill_in 'Last name', with: l_name
         fill_in 'First name', with: f_name
+        if role then select role, from: 'Role' end
 		end
 		
 		def signin(email, password)
 		  fill_in 'Email', with: email 
 		  fill_in 'Password', with: password
 		end 
-		
-		
 
 	end
 end
