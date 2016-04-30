433741ab58f321f9a2e2fe85e81bbe216ad89f6a
noradiegwu <nneomaoradiegwu2019@u.northwestern.edu>
2016-04-30 19:25:02 +0000

    Created model and factory for comments, as well as model spec tests.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
-# describe Comment, type: :model do
-#     it 'has a factory' do
-#         expect(FactoryGirl.create(:comment)).to be_valid
-#     end
+describe Comment, type: :model do
+    it 'has a factory' do
+        expect(FactoryGirl.create(:comment)).to be_valid
+    end
     
-#     it 'is invalid without a user' do
-#         expect(FactoryGirl.build(:comment, user: nil)).to_not be_valid
-#     end
+    it 'is invalid without a user' do
+        expect(FactoryGirl.build(:comment, user: nil)).to_not be_valid
+    end
     
-#     it 'is invalid without a body' do
-#         expect(FactoryGirl.build(:comment, body: nil)).to_not be_valid
-#         expect(FactoryGirl.build(:comment, body: '')).to_not be_valid
-#     end
-# end
+    it 'is invalid without a body' do
+        expect(FactoryGirl.build(:comment, body: nil)).to_not be_valid
+        expect(FactoryGirl.build(:comment, body: '')).to_not be_valid
+    end
+    
+    it 'is invalid without a question' do
+        expect(FactoryGirl.build(:comment, question: nil)).to_not be_valid
+    end
+end
