be861b02074d9740ef91ab22646578acd0360154
noradiegwu <nneomaoradiegwu2019@u.northwestern.edu>
2016-04-30 19:23:24 +0000

    Updated question and user models to have comments.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :questions
+  has_many :questions 
+  has_many :comments
   
   enum role: [:admin, :student]
 	before_validation :set_default_role
