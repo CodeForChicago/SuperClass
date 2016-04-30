1803142d587d2644934c4dcde063660d59fd57f1
Steven Layne <stevenzmanx@u.northwestern.edu>
2016-04-30 19:14:34 +0000

    updated feedback controller

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,19 +1,28 @@
 require "pry"
 
 class FeedbackController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  include Devise::Controllers::Helpers
+  helper_method :current_user
+  
   def new
     @feedback = Feedback.new
   end
   
   def create
-    @feedback = Feedback.new(params[:feedback])
-    @feedback.request = request
-    if @feedback.deliver
-      flash.now[:notice] = 'We appreciate your feedback! We will review your
-                            message and try to improve our website soon.'
-    else
-      flash.now[:error] = 'Cannot send message.'
-      render :new
-    end
-  end
-end
+      @feedback = Feedback.new(params[:feedback])
+      binding.pry
+      if current_user != nil
+        binding.pry
+      end 
+        @feedback["user"] = user
+      @feedback.request = request
+      if @feedback.deliver
+        flash.now[:notice] = 'We appreciate your feedback! We will review your
+                              message and try to improve our website soon.'
+      else
+        flash.now[:error] = 'Cannot send message.'
+        render :new
+      end
+  end 
+end 
