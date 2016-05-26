8b51f53bcfe91f1c68c1cc28a4682fc3abe5bc4f
Akshay Batra <akshaybatra2018@u.northwestern.edu>
2016-05-26 02:26:09 +0000

    Finished test for org leader to create a new student

--- spec/features/orgleader_add_feature_spec.rb
+++ spec/features/orgleader_add_feature_spec.rb
@@ -5,17 +5,37 @@ require 'spec_helper'
 
 feature 'Add Student', :devise do
   
+  f_name = "Codeniferous"
+	l_name = "Chicageous"
+	password = "cfcrulez"
+	email = "datcfc@gmail.com"
+	role = nil
+	
+	student_f_name = "awesome"
+	student_l_name = "pants"
+	student_password = "password"
+	student_email = "cfcstudent@gmail.com"
+	student_role = nil
+    
   # see spec/features/visitors_features_spec.rb for how to create a user both
   # with and without a orgleader role
   # we should be able to create the user, then access it from the session
   # store with User.method
   scenario 'if you are org leader then you can add student' do 
-    visit new_student_path
-    # binding.pry
-    expect(current_user.org_leader?).to be_true
-    click_button 'Add Student'
+    binding.pry
+    visit new_user_registration_path
+		sign_up_with(email, password, password, f_name, l_name, 'Organization Leader')
+ 		click_button 'Sign up'
+    #if ((User.first.role) is 'Organization Leader')
+    if (expect(User.where(email: email).first.role).to eq("org_leader"))
+      visit new_student_path
+      click_button 'Add Student'
+      sign_up_with(student_email, student_password, student_password, student_f_name,
+                   student_l_name, 'Student')
+      click_button 'Create Student' # make sure this matches the button's text
+      expect(User.where(email: student_email).first.email).to eq(student_email)
+    end 
     #Discuss with Juan what we get when we select roles
-    expect(page.status_code).to eq(200)
   end 
   
   scenario 'if you are not org leader then cant access page' do
@@ -25,6 +45,5 @@ feature 'Add Student', :devise do
     expect(current_user.org_leader?).to be_false
     click_button 'Add Student'
     expect(page.status_code).to eq(404)
-  end 
-  
+  end
 end 
--- spec/features/sessions/visitors_features_spec.rb
+++ spec/features/sessions/visitors_features_spec.rb
@@ -10,10 +10,10 @@ feature 'Sign Up' do
 	role = nil
     
     scenario 'visitor can sign up correctly as default student' do
-        visit new_user_registration_path
-		sign_up_with(email, password, password, f_name, l_name, role)
- 		click_button 'Sign up'
- 		txts = [I18n.t( 'devise.registrations.signed_up'), I18n.t( 'devise.registrations.signed_up_but_unconfirmed')]
+      visit new_user_registration_path
+			sign_up_with(email, password, password, f_name, l_name, role)
+ 			click_button 'Sign up'
+ 			txts = [I18n.t( 'devise.registrations.signed_up'), I18n.t( 'devise.registrations.signed_up_but_unconfirmed')]
   		expect(page).to have_content(/.*#{txts[0]}.*|.*#{txts[1]}.*/)
   		expect(User.where(email: email).first.role).to eq("student")
     end
