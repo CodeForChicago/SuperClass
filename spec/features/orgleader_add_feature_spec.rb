7a20f48fd466bb28396e6f2056af9770e1c6b058
BenSandeen <benjaminsandeen2016@u.northwestern.edu>
2016-05-26 02:48:55 +0000

    improving formatting

--- spec/features/orgleader_add_feature_spec.rb
+++ spec/features/orgleader_add_feature_spec.rb
@@ -4,19 +4,19 @@ require 'spec_helper'
 # Feature: If user is orgleader then user can add student
 
 feature 'Add Student', :devise do
-  
+
   f_name = "Codeniferous"
 	l_name = "Chicageous"
 	password = "cfcrulez"
 	email = "datcfc@gmail.com"
 	role = nil
-	
+
 	student_f_name = "awesome"
 	student_l_name = "pants"
 	student_password = "password"
 	student_email = "cfcstudent@gmail.com"
 	student_role = nil
-    
+
   # see spec/features/visitors_features_spec.rb for how to create a user both
   # with and without a orgleader role
   # we should be able to create the user, then access it from the session
@@ -34,10 +34,10 @@ feature 'Add Student', :devise do
                    student_l_name, 'Student')
       click_button 'Create Student' # make sure this matches the button's text
       expect(User.where(email: student_email).first.email).to eq(student_email)
-    end 
+    end
     #Discuss with Juan what we get when we select roles
-  end 
-  
+  end
+
   scenario 'if you are not org leader then cant access page' do
     visit new_student_path
     # binding.pry
@@ -46,4 +46,4 @@ feature 'Add Student', :devise do
     click_button 'Add Student'
     expect(page.status_code).to eq(404)
   end
-end 
+end
