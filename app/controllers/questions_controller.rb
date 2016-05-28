ef16d7e34a2f5bfb976ec1d6fbe396d739e7d09f
agam gupta <agamgupta2018@u.northwestern.edu>
2016-05-28 17:44:29 +0000

    fixing the user connection errors

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,7 +17,7 @@ class QuestionsController < ApplicationController
     
     def create
         @question = Question.new(question_params)
-        #binding.pry
+        # binding.pry
         @question.save!
         redirect_to questions_path
     end
@@ -25,6 +25,12 @@ class QuestionsController < ApplicationController
 private
     def question_params
         in_params = params.require(:question)
+        user_id = in_params[:user].to_i
+        # find the user given a particular id
+        selected_user = User.find(user_id)
         in_params.permit(:user, :title, :body)
+        out_params = in_params.permit(:title, :body)
+        #binding.pry
+        #out_params[:user] = user_id#selected_user 
     end
 end
