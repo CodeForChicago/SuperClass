b785650c4ef7bf33b6aa2a85f96903018d775389
Steven Layne <stevenzmanx@u.northwestern.edu>
2016-08-30 16:08:20 +0000

    Added failsafe for username generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,19 +17,27 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 	validates :first_name, :last_name, :role, :username, presence: true
 	#validates :username, uniqueness: true
-	validates :username, :presence => true, :uniqueness => {:case_sensitive => false}
+	validates :username, :presence => true, :uniqueness => true 
 	def set_default_role
 	  self.role ||= :student
 	end 
 	
+  def create
+    @user = User.new(params[:user])
+    @user.save!
+  rescue => err                          # rescues all exceptions
+    logger.error(err.to_s) #This is to catch an error in the case that in the future we have multiple deployments working and they both request the same random user at the same time. Very unlikely edge case. Failsafe nonetheless. 
+  end 
+  
 	def fetch_user
 	  	res = JSON.parse(open('https://randomuser.me/api/').read)
 	    res['results'][0]['login']['username']
 	end 
 	
   def generate_username
-    begin  #The email retrieved may on rare occasions already exist. So we need to catch cases where the API serves a user we have already used
+    begin  #The username generated may on rare occasions already exist. So we need to catch cases where the API serves a user we have already used
       self.username = fetch_user()
+
     rescue # If the appointment to self.username is not unqieu the model will throw an error. If an error is thrown we just call generate username again until we get a unique username. 
       generate_username()
     end
